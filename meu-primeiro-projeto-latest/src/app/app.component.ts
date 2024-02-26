0f4a98af3a4b7e7a7a27ea7f12587eea7831ae63
Ryan Silva <ryan.silva@b2ml.com.br>
2024-02-26 11:49:01 -0300

    Add new archieves

--- meu-primeiro-projeto-latest/src/app/app.component.ts
+++ meu-primeiro-projeto-latest/src/app/app.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { NewComponent } from './components/new-component/new-component.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  imports: [CommonModule, RouterOutlet, NewComponent],
+  template: `
+  <!-- <router-outlet></router-outlet>, -->
+    <h1>Curso de Angular</h1>
+    <h2>Componentes</h2>
+    <app-new-component/>
+  `,
 })
 export class AppComponent {
   title = 'meu-primeiro-projeto-latest';
